b774351b870bd0d8307b3756eec62a8e522226f2
charliewambo31 <111303001+charliewambo31@users.noreply.github.com>
2023-03-03 09:29:53 -0500

    commit

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-let userinput
-switch(true) {
-  case userinput <= 10:
-    alert("Less than 10");
-    break;
+// let userinput
+// switch(true) {
+//   case userinput <= 10:
+//     alert("Less than 10");
+//     break;
 
-  case userinput > 10:
-    alert("Greater than 10");
-    break;
+//   case userinput > 10:
+//     alert("Greater than 10");
+//     break;
 
-  default:
-    alert("Please input value");
-    break;
-}
+//   default:
+//     alert("Please input value");
+//     break;
+// }
+
+
+
+
+
+
+
+const selectElement1 = document.querySelector('.choix1');
+selectElement1.addEventListener('change', (event) => {
+var result1 = document.querySelector('.clinique1');
+  result1.textContent = `Votre choix est : ${event.target.value}`;
+});
 
 
 const myArray = ["apple", "banana", "orange", "grape", "pear"];
@@ -20,41 +32,50 @@ const randomElement = myArray[randomIndex];
 console.log(randomElement);
 
 
+// const bouton = document.getElementById('button');
+// bouton.addEventListener('click', () => {
+//   alert('Le bouton a été cliqué !');
+// });
 
 
-const selectElement1 = document.querySelector('.choix1');
-selectElement1.addEventListener('change', (event) => {
-var result1 = document.querySelector('.clinique1');
-  result1.textContent = `La clinique choisi est : ${event.target.value}`;
+
+
+const bouton1 = document.getElementById('button');
+bouton1.addEventListener('click', () => {
+  const choix = Math.random();
+  if (choix < 0.1) {
+    alert('Vous avez gagne 1000$');
+  } else if  (choix < 0.3) {
+    alert('vous avez gagne 100$');
+  } else if  (choix < 0.6) {
+    alert('vous avez gagne un autre cafe');
+  } else {
+    alert('vous avez perdu');
+  }
 });
 
 
 
 
 
+// function inputIQFunction() {
+//     var userinput = document.getElementById("inputIQ").value;
+//     userinput = parseInt(userinput);
+//     switch (true) {
 
+//         case userinput <= 10:
+//             alert("Less than 10");
+//             break;
 
+//         case userinput > 10:
+//             alert("Greater than 10");
+//             break;
 
-
-
-function inputIQFunction() {
-    var userinput = document.getElementById("inputIQ").value;
-    userinput = parseInt(userinput);
-    switch (true) {
-
-        case userinput <= 10:
-            alert("Less than 10");
-            break;
-
-        case userinput > 10:
-            alert("Greater than 10");
-            break;
-
-        default:
-            alert("Please input value");
-            break;
-      }
-  }
+//         default:
+//             alert("Please input value");
+//             break;
+//       }
+//   }
 
 
   
